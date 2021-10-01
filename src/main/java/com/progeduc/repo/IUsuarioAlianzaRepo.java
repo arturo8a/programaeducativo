596ac5c756c52c1adba82393e4141665a5f663a9
Jorge <hernandezs.jv@gmail.com>
2021-10-01 17:36:50 -0500

    login usuario evaluador por año

--- src/main/java/com/progeduc/repo/IUsuarioAlianzaRepo.java
+++ src/main/java/com/progeduc/repo/IUsuarioAlianzaRepo.java
@@ -17,7 +17,7 @@ public interface IUsuarioAlianzaRepo extends CrudRepository<UsuarioAlianza,Integ
 	@Query(value="SELECT TB1.* FROM USUARIO_ALIANZA TB1 WHERE TB1.odsid = ?1 ",nativeQuery = true)
 	List<UsuarioAlianza> listarByOds(Integer odsid);
 	
-	@Query(value="SELECT U.* FROM USUARIO_ALIANZA U WHERE U.usuario_autoridad = ?1 and U.password_autoridad=?2 and U.estado=1",nativeQuery = true)
+	@Query(value="SELECT U.* FROM USUARIO_ALIANZA U WHERE U.usuario_autoridad = ?1 and U.password_autoridad=?2 and U.estado=1 and U.anio = EXTRACT(YEAR FROM sysdate) ",nativeQuery = true)
 	UsuarioAlianza getUsuarioEvaluador(String usuario, String password);
 	
 	@Query(value="select U.* from USUARIO_ALIANZA U where U.odsid like %?1% and U.anio like %?2% and U.estado like %?3% and U.COMITE_TECNICO like %?4%  and U.COMITE_EVALUADOR like %?5%  and U.AUSPICIADOR like %?6%  and U.ALIADO like %?7%   ",nativeQuery = true)
